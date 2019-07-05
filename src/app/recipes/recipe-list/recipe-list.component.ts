9ddab4b366661ef274d5e1ddb12f1b15888c94ff
Jim Kennedy <jgkennedy@rentworx.com>
2019-07-05 00:00:20 -0400

    Fixed up the recipe list component.  Still much work to do.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,13 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe1', 'Test',
+      'https://thumbs.dreamstime.com/z/fresh-italian-pizza-food-photography-design-' +
+      'top-view-photo-mexican-pizza-chips-onion-hot-jalapeno-pepper-fresh-italian-119192317.jpg'),
+    new Recipe('Test Recipe2', 'Test two',
+      'https://simple-nourished-living.com/wp-content/uploads/2008/10/hamburger-goulash-american-chop-suey.jpg')
+  ];
 
   constructor() {
   }
