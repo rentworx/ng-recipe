34c64260741bd9dc31b973a988e74fa719515239
Jim Kennedy <jgkennedy@rentworx.com>
2019-07-06 22:57:59 -0400

    Recipe project more complete now with databinding.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.compo
 import {RecipeItemComponent} from './recipes/recipe-list/recipe-item/recipe-item.component';
 import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit
     ShoppingEditComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
