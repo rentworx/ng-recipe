cad4100d4709642b155fb0ff8fb5c794d92d2699
Jim Kennedy <jgkennedy@rentworx.com>
2019-08-12 22:54:24 -0400

    Add changes related to course module section 10 (services).  Use of services is a better approach than communicating with lots of events.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
+import {RecipeService} from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,23 +8,13 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[];
 
-  recipes: Recipe[] = [
-    new Recipe('Test Recipe1', 'Test',
-      'https://thumbs.dreamstime.com/z/fresh-italian-pizza-food-photography-design-' +
-      'top-view-photo-mexican-pizza-chips-onion-hot-jalapeno-pepper-fresh-italian-119192317.jpg'),
-    new Recipe('Test Recipe2', 'Test two',
-      'https://simple-nourished-living.com/wp-content/uploads/2008/10/hamburger-goulash-american-chop-suey.jpg')
-  ];
-
-  constructor() {
+  constructor(private recipeService: RecipeService) {
   }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,37 @@
+import {Recipe} from './recipe.model';
+import {EventEmitter, Injectable} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe('Test Recipe1', 'Test',
+      'https://thumbs.dreamstime.com/z/fresh-italian-pizza-food-photography-design-' +
+      'top-view-photo-mexican-pizza-chips-onion-hot-jalapeno-pepper-fresh-italian-119192317.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20),
+        new Ingredient('Breadcrumbs', 10)
+      ]),
+    new Recipe('Test Recipe2', 'Test two',
+      'https://simple-nourished-living.com/wp-content/uploads/2008/10/hamburger-goulash-american-chop-suey.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
+  ];
+
+  constructor(private shoppingListService: ShoppingListService) {
+  }
+
+  getRecipes(): Recipe[] {
+    return this.recipes.slice(); // return a copy of the array
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
+  }
+}
