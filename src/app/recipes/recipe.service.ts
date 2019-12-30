0188eba1a49ca4fc0324c11762be941ac3f51044
Jim Kennedy <jgkennedy@rentworx.com>
2019-12-30 15:03:56 -0500

    Completed http section for the course project.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,29 +8,30 @@ import {Subject} from 'rxjs';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe('Test Recipe1', 'Test',
-      'https://img.buzzfeed.com/thumbnailer-prod-us-east-1/dc23cd051d2249a5903d25faf8eeee4c/' +
-      'BFV36537_CC2017_2IngredintDough4Ways-FB.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20),
-        new Ingredient('Breadcrumbs', 10)
-      ]),
-    new Recipe('Test Recipe2', 'Test two',
-      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2004/2/25/0/' +
-      'bw2b07_hambugers1.jpg.rend.hgtvcom.826.620.suffix/1558017418187.jpeg',
-      [
-        new Ingredient('Buns', 2),
-        new Ingredient('Meat', 1)
-      ]),
-    new Recipe('Speghetti', 'With meat sauce',
-      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/homemade-spaghetti-sauce-horizontal-1530890913.jpg',
-      [
-        new Ingredient('Speghetti', 2),
-        new Ingredient('Tomatoes', 5)
-      ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe('Test Recipe1', 'Test',
+  //     'https://img.buzzfeed.com/thumbnailer-prod-us-east-1/dc23cd051d2249a5903d25faf8eeee4c/' +
+  //     'BFV36537_CC2017_2IngredintDough4Ways-FB.jpg',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('French Fries', 20),
+  //       new Ingredient('Breadcrumbs', 10)
+  //     ]),
+  //   new Recipe('Test Recipe2', 'Test two',
+  //     'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2004/2/25/0/' +
+  //     'bw2b07_hambugers1.jpg.rend.hgtvcom.826.620.suffix/1558017418187.jpeg',
+  //     [
+  //       new Ingredient('Buns', 2),
+  //       new Ingredient('Meat', 1)
+  //     ]),
+  //   new Recipe('Speghetti', 'With meat sauce',
+  //     'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/homemade-spaghetti-sauce-horizontal-1530890913.jpg',
+  //     [
+  //       new Ingredient('Speghetti', 2),
+  //       new Ingredient('Tomatoes', 5)
+  //     ])
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {
   }
@@ -49,6 +50,11 @@ export class RecipeService {
 
   addRecipe(recipe: Recipe) {
     this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
     this.recipesChanged.next(this.recipes.slice());
   }
 
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {RecipeService} from '../recipes/recipe.service';
+import {Recipe} from '../recipes/recipe.model';
+import {map, tap} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataStorageService {
+  constructor(private httpClient: HttpClient, private recipesService: RecipeService) {
+  }
+
+  storeRecipes(): void {
+    const recipes = this.recipesService.getRecipes();
+    this.httpClient.put('https://ng8-recipe-book-6c470.firebaseio.com/recipes.json', recipes)
+      .subscribe(response => {
+        console.log('PUT response: ', response);
+      });
+  }
+
+  fetchRecipes(): Observable<Recipe[]> {
+    return this.httpClient.get<Recipe[]>('https://ng8-recipe-book-6c470.firebaseio.com/recipes.json')
+      .pipe(
+        map((recipes: Recipe[]) => {
+          return recipes.map(recipe => {
+            return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+          });
+        }),
+        tap(recipes => {
+          this.recipesService.setRecipes(recipes);
+        }));
+  }
+}
