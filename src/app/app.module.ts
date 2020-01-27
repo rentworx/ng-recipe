05161d069eba8038006bf2028fc900503f9f0854
Jim Kennedy <jgkennedy@rentworx.com>
2020-01-26 22:24:51 -0500

    Added Core module containing services.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,14 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
-import {RecipeService} from './recipes/recipe.service';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 import {AuthComponent} from './auth/auth.component';
-import {AuthInterceptorService} from './auth/auth-interceptor.service';
 import {RecipesModule} from './recipes/recipes.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
 import {CommonModule} from '@angular/common';
 import {SharedModule} from './shared/shared.module';
+import {CoreModule} from './core.module';
 
 @NgModule({
   declarations: [
@@ -30,14 +28,9 @@ import {SharedModule} from './shared/shared.module';
     RecipesModule,
     ShoppingListModule,
     CommonModule,
-    SharedModule
+    SharedModule,
+    CoreModule
   ],
-  providers: [ShoppingListService, RecipeService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true
-    }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
